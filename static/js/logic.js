fc3246042094e9c0dac98c9794d60b91b54ae8dc
Marta Woodkowski <martawoodkowski@gmail.com>
2021-10-05 00:28:54 -0700

    Carto Map

--- static/js/logic.js
+++ static/js/logic.js
@@ -76,9 +76,11 @@ d3.json(url).then(function(data) {
     // var grayscale = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
     //   attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
     // });
-    var grayscale = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
-	  maxZoom: 20,
-	  attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+    
+    var grayscale = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      subdomains: 'abcd',
+      maxZoom: 19
     });
     
     // Tried another 'grayscale' (requires API, not best when doing GitHub pages)->https://maps.omniscale.com/en/examples/leaflet_grayscale
